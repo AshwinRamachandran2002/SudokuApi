2903d92ee76a52bb43a9a976673af3ce3a0419f8
Ashwin <ashwinramachandrang@gmail.com>
2021-05-27 13:02:00 +0530

    l

--- digitrecognizer/main/views.py
+++ digitrecognizer/main/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render
+import numpy as np # linear algebra
+import cv2
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dropout, Dense, Flatten, BatchNormalization, Conv2D, MaxPooling2D
+from tensorflow.keras.optimizers import RMSprop,Adam
+from tensorflow.keras import backend as K
+from tensorflow.keras.preprocessing import image
 def predict(request):
-    # model=tf.keras.models.load_model("./anoth_model11.h5")
-    # result=0
-    # imgData = cv2.imread('./img.png')
-    # img = np.asarray(imgData)
-    # img=img.reshape(1,28,28,1)
-    # predictions = model.predict(img)
-    # classIndex = model.predict_classes(img)
-    # probabilityValue = np.amax(predictions)
-    
-    # if probabilityValue > 0.3:
-    #     result=(classIndex[0])
-    # else:
-    #     result=(0)
+    model=tf.keras.models.load_model("./anoth_model11.h5")
     result=0
+    imgData = cv2.imread('./img.png')
+    img = np.asarray(imgData)
+    img=img.reshape(1,28,28,1)
+    predictions = model.predict(img)
+    classIndex = model.predict_classes(img)
+    probabilityValue = np.amax(predictions)
+    
+    if probabilityValue > 0.3:
+        result=(classIndex[0])
+    else:
+        result=(0)
     return (result)
 def index(request):
     digit=predict(request)
-    digit=0
     return render(request, 'index.html', {'digit':digit})
 # urls.py
 from django.urls import path
