3460df5db7b13297784ae6641223c191a2e89833
Ashwin <ashwinramachandrang@gmail.com>
2021-05-27 13:00:47 +0530

    l

--- digitrecognizer/digitrecognizer/urls.py
+++ digitrecognizer/digitrecognizer/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
-from main.views import predict
-urlpatterns = [
-    path('', include('main.urls')),
-    path('api/predict/', predict)
+from django.urls import include,path
+#from main.views import predict
+urlpatterns= [
+    path('admin/', admin.site.urls),
+    path('', include('main.views')),
 ]
--- digitrecognizer/main/views.py
+++ digitrecognizer/main/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
-
+def predict(request):
+    # model=tf.keras.models.load_model("./anoth_model11.h5")
+    # result=0
+    # imgData = cv2.imread('./img.png')
+    # img = np.asarray(imgData)
+    # img=img.reshape(1,28,28,1)
+    # predictions = model.predict(img)
+    # classIndex = model.predict_classes(img)
+    # probabilityValue = np.amax(predictions)
+    
+    # if probabilityValue > 0.3:
+    #     result=(classIndex[0])
+    # else:
+    #     result=(0)
+    result=0
+    return (result)
 def index(request):
-    return render(request, 'index.html', {})
+    digit=predict(request)
+    digit=0
+    return render(request, 'index.html', {'digit':digit})
 # urls.py
 from django.urls import path
 from .views import index
