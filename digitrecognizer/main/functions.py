f8f2f81520f2a87495590e1f8c8ac0824ffc1f52
Ashwin <ashwinramachandrang@gmail.com>
2021-05-27 11:48:10 +0530

    l

--- digitrecognizer/main/functions.py
+++ digitrecognizer/main/functions.py
@@ -1,28 +1,11 @@
 import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import math
-import numpy as np
-import h5py
-import matplotlib.pyplot as plt
-import scipy
-from PIL import Image
-from scipy import ndimage
 import tensorflow as tf
 from tensorflow.python.framework import ops
-%matplotlib inline
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Activation, Dropout, Dense, Flatten, BatchNormalization, Conv2D, MaxPooling2D
 from tensorflow.keras.optimizers import RMSprop,Adam
 from tensorflow.keras import backend as K
 from tensorflow.keras.preprocessing import image
-from sklearn.metrics import accuracy_score, classification_report
-import os, random
-import cv2
-from glob import glob
-import sklearn
-from sklearn.model_selection import train_test_split
-
- 
 
 def predict(request):
     model=tf.keras.models.load_model("./anoth_model11.h5")
